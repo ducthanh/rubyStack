bc6ec5edb4123714a48a6357fcadc01ad97b941b
Thanh Duc Nguyen <thanh.nguyen-duc@hellopay.com>
2016-12-24 00:40:37 +0700

    logger

--- trick.rb
+++ trick.rb
@@ -23,3 +23,54 @@ end
 a = ShoppingList.new(12,3,4,5,5)
 
 puts a.in.inspect
+# => [12, 3, 4, 5, 5]
+
+
+#Logger
+require 'logger'
+
+
+logger = Logger.new(STDOUT)
+
+# determine the log level
+logger.level = Logger::INFO
+
+logger.debug("Created logger")
+logger.info("Program started")
+
+logger.warn("Nothing to do!")
+logger.error "Argument #{@foo} mismatch."
+logger.add(Logger::FATAL) { 'Fatal error!' }
+logger.debug("this is debug")
+
+email = "log info"
+
+logger.info(email)
+
+
+path = "a_non_existent_file"
+
+# (\w+)? and (\w*) (0..+inf word characters)
+
+begin 
+  File.foreach(path) do |line|
+    unless line =~ /^(\w+)$/
+      logger.error("Line in wrong format: #{line.chomp}")
+    end
+  end
+rescue => err
+  logger.fatal("Caught exception; existing")
+  logger.fatal(err)
+end
+
+
+
+
+
+
+
+
+
+
+
+
